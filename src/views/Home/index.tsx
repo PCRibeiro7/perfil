8cdff536d98f5a097306671c15f830fa26fdf89f
Pedro Ribeiro <pribeiro@extensiv.com>
2023-04-29 01:20:24 -0300

    adds background music

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -6,6 +6,7 @@ import { sessionSlice } from '@/slices/session';
 import { Zoom } from '@mui/material';
 
 import Typewriter from 'typewriter-effect';
+import useSound from 'use-sound';
 
 const instructions = [
     'Descubra a palavra secreta',
@@ -16,9 +17,14 @@ const instructions = [
 ];
 
 export default function Home(): JSX.Element {
-    const isReady = useDelay(2000);
+    const [playSound] = useSound('/sounds/background.mp3', {
+        volume: 0,
+    });
+    const startButtonIsReady = useDelay(4000);
+    const instructionsIsReady = useDelay(1000);
     const session = sessionSlice.use();
     const startGame = () => {
+        playSound({});
         gameSlice.dispatch({
             type: IGameActions.CHANGE_PAGE,
             payload: { page: ICurrentPage.GAME },
@@ -26,26 +32,31 @@ export default function Home(): JSX.Element {
     };
     return (
         <main className="flex flex-col justify-evenly items-center h-screen bg-slate-200 center">
-            <h1 className="text-6xl text-black text-center">
-                Adivinha quem é?
-            </h1>
-            <div className="max-w-xs text-center h-56">
-                <div className="bg-white p-2 rounded-xl">
-                    <Typewriter
-                        options={{
-                            strings: [
-                                `Olá, ${session.user.name}`,
-                                ...instructions,
-                            ],
-                            autoStart: true,
-                            loop: true,
-                            wrapperClassName: 'text-3xl',
-                            deleteSpeed: 5,
-                        }}
-                    />
+            <Zoom in={true} timeout={1000}>
+                <div className="bg-white p-4 rounded-xl max-w-xs">
+                    <h1 className="text-6xl text-black">Perfil Online</h1>
                 </div>
+            </Zoom>
+            <div className="max-w-xs h-56">
+                {instructionsIsReady && (
+                    <div className="bg-white p-4 rounded-xl">
+                        <Typewriter
+                            options={{
+                                strings: [
+                                    `Olá, ${session.user.name}`,
+                                    ...instructions,
+                                ],
+                                autoStart: true,
+
+                                loop: true,
+                                wrapperClassName: 'text-3xl',
+                                deleteSpeed: 5,
+                            }}
+                        />
+                    </div>
+                )}
             </div>
-            <Zoom in={isReady} timeout={1000}>
+            <Zoom in={startButtonIsReady} timeout={1000}>
                 <button
                     onClick={startGame}
                     className="bg-black p-2 rounded-xl w-40  hover:bg-slate-600"
