36b261d5b3a2f0843d24185c3a3d87250d7d68f7
Pedro Ribeiro <pedro.costa.ribeiro@gmail.com>
2023-05-01 04:40:27 -0300

    fix import

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,5 +1,5 @@
 import { writeFileSync } from 'fs';
-import cards from '../src/assets/cards.json';
+import cards from '../src/frontend/assets/cards.json';
 import { ICard } from '@/frontend/models/game/ICard';
 import { randomUUID } from 'crypto';
 
