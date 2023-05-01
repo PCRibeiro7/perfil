6b46b912492d30997ebc38d80f11b5648c92c46c
Pedro Ribeiro <pedro.costa.ribeiro@gmail.com>
2023-05-01 00:02:11 -0300

    improves filtercards for user logic

--- src/utils/filterCardsForUser.ts
+++ src/utils/filterCardsForUser.ts
@@ -2,31 +2,30 @@ import { ICard } from '@/frontend/models/game/ICard';
 import { User } from '@prisma/client';
 
 export const filterCardsForUser = (cards: ICard[], user: User): ICard[] => {
+    const seenButNotAnsweredCards = cards.filter(
+        card =>
+            user.seenCardIds.includes(card.id) &&
+            !user.correctCardIds.includes(card.id) &&
+            !user.skippedCardIds.includes(card.id),
+    );
+
+    const skippedCards = cards.filter(card =>
+        user.skippedCardIds.includes(card.id),
+    );
+
     let cardsToHide = [
         ...user.correctCardIds,
         ...user.seenCardIds,
         ...user.skippedCardIds,
     ];
 
-    let filteredCards = cards.filter(card => !cardsToHide.includes(card.id));
+    let neverSeenBeforeCards = cards.filter(
+        card => !cardsToHide.includes(card.id),
+    );
 
-    if (filteredCards.length !== 0) {
-        return filteredCards;
-    }
-
-    cardsToHide = [...user.correctCardIds, ...user.skippedCardIds];
-    filteredCards = cards.filter(card => !cardsToHide.includes(card.id));
-
-    if (filteredCards.length !== 0) {
-        return filteredCards;
-    }
-
-    cardsToHide = [...user.correctCardIds];
-    filteredCards = cards.filter(card => !cardsToHide.includes(card.id));
-
-    if (filteredCards.length !== 0) {
-        return filteredCards;
-    }
-
-    return [];
+    return [
+        ...seenButNotAnsweredCards,
+        ...neverSeenBeforeCards,
+        ...skippedCards,
+    ];
 };
